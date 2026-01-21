16dc895b38aaed5fce34c24f48e627f0fbf37c72
Dominik Lange <langedominik51@gmail.com>
2026-01-21 17:09:06 +0100

    bugfix: bei mehreren gefüllten textfeldern wurde nur 1 gespeichert

--- pokedex.js
+++ pokedex.js
@@ -1,28 +1,28 @@
-
-function attachNoteField(pNotes, id) {
-//  Lade gespeicherte Notizen
-const notes = JSON.parse(localStorage.getItem("notes") || "{}");
-
-// Textarea erstellen
+function attachNoteField(element, id) {
+// Textfeld erstellen
 const textarea = document.createElement("textarea");
-textarea.placeholder = "Was macht dieses Pokémon besonders für dich...";
-textarea.value = notes[id] || "";
+textarea.placeholder = "Deine persönliche Notiz...";
 
-// Styling kann gerne abgeändert werden :D
+// Styling gerne abändern :D
 textarea.style.width = "100%";
 textarea.style.height = "60px";
 textarea.style.marginTop = "5px";
 
-// Eingaben bei jeder Änderung speichern
+// Beim Erstellen gespeicherte Notiz laden
+const savedNotes = JSON.parse(localStorage.getItem("notes") || "{}");
+textarea.value = savedNotes[id] || "";
+
+// Bei jeder Eingabe speichern
 textarea.addEventListener("input", () => {
-    notes[id] = textarea.value;
-    localStorage.setItem("notes", JSON.stringify(notes));
+    // Immer die aktuelle Version aus localStorage holen
+    const currentNotes = JSON.parse(localStorage.getItem("notes") || "{}");
+    currentNotes[id] = textarea.value; // nur diese ID updaten
+    localStorage.setItem("notes", JSON.stringify(currentNotes));
 });
 
-// Textarea zum Element hinzufügen
-pNotes.appendChild(textarea);
+// Textfeld zum Ziel-Element hinzufügen
+element.appendChild(textarea);
 }
-
 
 
 // BEISPIEL kann auskommentiert werden oder gelöscht
@@ -31,12 +31,18 @@ const card1 = document.createElement("div");
 card1.textContent = "Karte 1";
 document.body.appendChild(card1);
 
+
 const card2 = document.createElement("div");
 card2.textContent = "Karte 2";
 document.body.appendChild(card2);
+
+
+const card3 = document.createElement("div");
+card3.textContent = "Karte 3";
+document.body.appendChild(card3);
 //BEISPIEL ENDE
 
-// Das muss an jede Karte gehängt werden
-// Textfeld an beide Karten anhängen
+// Das muss an jede Karte gehängt werden (Wenn ich das richitg verstehe :D)
 attachNoteField(card1, "karte1");
 attachNoteField(card2, "karte2");
+attachNoteField(card3, "karte3");
